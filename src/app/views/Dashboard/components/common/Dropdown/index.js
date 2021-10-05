72679250826dcfee458b0dd05df53d4c621be83c
pislagz <77860645+pislagz@users.noreply.github.com>
2021-10-05 22:39:44 +0200

    fix: dropdown contents unmount on collapse

--- src/app/views/Dashboard/components/common/Dropdown/index.js
+++ src/app/views/Dashboard/components/common/Dropdown/index.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import { MoreLessIcon } from "assets/icons/functional/MoreLessIcon";
 import { Wrapper, Content, Text, Activator, IconWrapper } from "./styled";
-import { Collapse } from "react-collapse";
+import { UnmountClosed } from "react-collapse";
 
 export const Dropdown = ({ children }) => {
   const [isCollapsed, setIsCollapsed] = useState(true);
 
   return (
     <Wrapper>
-      <Collapse isOpened={!isCollapsed}>
+      <UnmountClosed isOpened={!isCollapsed}>
         <Content>{children}</Content>
-      </Collapse>
+      </UnmountClosed>
       <Activator onClick={() => setIsCollapsed(!isCollapsed)}>
         <IconWrapper isDown={isCollapsed}>
           <MoreLessIcon />
