1d32836012826419d6af398ac3b7a2295fc12805
pislagz <pislax_flash@yahoo.com>
2021-11-30 17:31:00 +0100

    feat: history

--- src/app/views/History/index.js
+++ src/app/views/History/index.js
@@ -1,9 +1,15 @@
+import "./timeline.scss";
+import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchHistoryData } from "app/redux/actions/history";
 import { selectHistory } from "app/redux/selectors";
 import { Error } from "app/components/common/Error";
 import { Loading } from "app/components/common/Loading";
 import { useEffect } from "react";
+import { Chrono } from "react-chrono";
+import { DateTime } from "luxon";
+import { Flex } from "app/components/common/ui";
+import { colors } from "app/styles/theme/colors";
 
 export const History = () => {
   const dispatch = useDispatch();
@@ -19,7 +25,71 @@ export const History = () => {
     <>
       {status === "loading" && <Loading />}
       {status === "failed" && <Error />}
-      {status === "success" && "history"}
+      {status === "success" && (
+        <>
+          <Chrono
+            mode="VERTICAL_ALTERNATING"
+            borderLessCards
+            useReadMore={false}
+            hideControls
+            cardWidth="220"
+            cardHeight="150"
+            theme={{
+              primary: "white",
+              secondary: "white",
+              cardBgColor: "transparent",
+              cardForeColor: "white",
+              titleColor: "white",
+            }}
+          >
+            {data.map((it) => (
+              <Item
+                key={it.title}
+                href={
+                  // exclude spacex links as they're not working anymore
+                  it.links.article?.includes("spacex.com")
+                    ? null
+                    : it.links.article
+                }
+                target="_blank"
+              >
+                <Title>
+                  {DateTime.fromISO(it.event_date_utc).toFormat("dd LLL yyyy")}
+                </Title>
+                <Desc>{it.title}</Desc>
+              </Item>
+            ))}
+          </Chrono>
+        </>
+      )}
     </>
   );
 };
+
+const Item = styled(Flex).attrs({ as: "a" })`
+  color: ${colors.white};
+  flex-direction: column;
+  cursor: ${({ href }) => (href ? "pointer" : "default")};
+  text-decoration: none;
+
+  &:hover {
+    p {
+      text-decoration: ${({ href }) => (href ? "underline" : "none")};
+      transition: all 0.2s ease-out;
+    }
+  }
+`;
+const Title = styled(Flex).attrs({
+  as: "h2",
+  m: "0",
+  p: "0",
+  fontSize: "md",
+  fontWeight: "500",
+})``;
+const Desc = styled(Flex).attrs({
+  as: "p",
+  m: "0",
+  p: "0",
+  fontSize: "md",
+  fontWeight: "100",
+})``;
