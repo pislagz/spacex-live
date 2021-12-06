6f45ff97d3b4c7b4ff9f92ba5d39b9cfb1ca80ba
pislagz <pislax_flash@yahoo.com>
2021-12-06 18:08:18 +0100

    fix: render

--- src/app/views/Starlink/index.js
+++ src/app/views/Starlink/index.js
@@ -7,12 +7,13 @@ import { Desktop } from "./components/Desktop";
 export const Starlink = () => {
   const bp = useBreakpoint();
   const isBigScreen =
-    bp === "lg" || bp === "xl" || bp === "2xl" || bp === "max";
+    bp === "" || bp === "lg" || bp === "xl" || bp === "2xl" || bp === "max";
   const [isGlobeVisible, setIsGlobeVisible] = useState(isBigScreen);
 
   useEffect(() => {
+    console.log({ bp });
     setIsGlobeVisible(isBigScreen);
-  }, [isBigScreen]);
+  }, [isBigScreen, isGlobeVisible, bp]);
 
   return isGlobeVisible ? <Desktop bp={bp} /> : <Mobile />;
 };
